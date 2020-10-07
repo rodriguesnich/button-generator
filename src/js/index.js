8ecdec982831d6d9324029cdefe0512ad479ad65
Nicholas <rodriguesnich@outloo.com>
2020-10-07 08:51:02 -0300

    :rocket: jquery.css on change feature
    this commit implements the number input of each button property onchange event changes the button

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,37 @@
 // alert('Roi')
+var button = $('.but')
+button.css("border-style", "solid")
 
 // Width Listner and Setter
 $("#width").bind('keyup mouseup', function () {
-    // alert("changed");            
-    let value = $(this).val()
-    console.log("width: " + value);
+    let component = $(this)
+    let size = component.val().toString()
+    button.css("width", size)
+    console.log("width: " + component.val() + "| size: " + size)
 });
 
 // Heigth Listner and Setter
 $("#heigth").bind('keyup mouseup', function () {
-    // alert("changed");            
-    let value = $(this).val()
-    console.log("height: " + value);
+    let component = $(this) 
+    let size = component.val().toString()
+    button.css("height", size)  
+    console.log("height: " + component.val());
 });
 
 // Border Listner and Setter
 $("#border").bind('keyup mouseup', function () {
     // alert("changed");            
-    let value = $(this).val()
-    console.log("border: " + value);
+    let component = $(this)
+    let size = component.val().toString()
+    button.css("border-width", size)
+    console.log("border: " + component.val());
 });
 
 // Radius Listner and Setter
 $("#radius").bind('keyup mouseup', function () {
     // alert("changed");            
-    let value = $(this).val()
-    console.log("radius: " + value);
+    let component = $(this)
+    let size = component.val().toString() + "px"
+    button.css("border-radius", size)
+    console.log("radius: " + component.val());
 });
